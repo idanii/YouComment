ba70d244fae5e9c72d57621dfbe469ea82ff81f2
Idan <66463744+idanii@users.noreply.github.com>
2021-01-18 11:17:55 +0200

    Update firebase.utils.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyA_JU1lok6qYtuuNkaOFVsMsjXtNrOGd08",
-  authDomain: "project-8654b.firebaseapp.com",
-  projectId: "project-8654b",
-  storageBucket: "project-8654b.appspot.com",
-  messagingSenderId: "8198479419",
-  appId: "1:8198479419:web:ae959b9e2c27ef9055461e",
-  measurementId: "G-11NLSCFFGH",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 firebase.initializeApp(config);
